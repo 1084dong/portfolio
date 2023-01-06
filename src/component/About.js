35255b74670baad4f2ee2e067c73a0ccfdd9c0cf
dong <1084dong@naver.com>
2023-01-06 15:44:21 +0900

    recent-update

--- src/component/About.js
+++ src/component/About.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 function About(props) {
     return (
-        <div>
-            여기는 about me 입니다.
+        <div className="common-background">
+            <div className="common-content d-flex-column">
+                <h1>About Me</h1>
+                <div id="col-1" className="d-flex-row justify-content-around">
+                    <div className="about-1">
+                        <img className="mypicture" src="/icons/hdj.jpg"/>
+                    </div>
+                    <div className="about-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, placeat.
+                    </div>
+                    <div className="about-3">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, exercitationem.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
